b532b96644fbdbd1506e8032fcf49d4e5c611622
bing xu <bingxu.au@gmail.com>
2021-04-28 12:41:52 +1000

    fixed an issue with dropping too much stock data and finished refactoring PCA function

--- src/stocks.py
+++ src/stocks.py
@@ -35,9 +35,36 @@ def build_returns_dataframe(stock_prices: list) -> pd.DataFrame:
     '''
     df = reduce(lambda left, right: pd.merge(left, right, left_index=True, right_index=True, how='outer'), stock_prices)
     returns = df.pct_change()
-    returns = returns.dropna(axis = 0)
+    returns.drop(df.head(1).index, inplace=True)
+    before = returns.shape
+    returns = returns.dropna(axis = 1)
+    print("Dropped columns with NaNs, ", before, " -> ", returns.shape)
     return returns
 
+
+def perform_pca(returns_df: pd.DataFrame, n):
+    '''
+    Uses the sklearn library to perform principle components analysis on the stocks.
+    '''
+    stocks = returns_df.columns
+    stdev_returns = returns_df.std(ddof=1, axis=0)
+    # Standardise the data
+    scaled = StandardScaler().fit_transform(returns_df)
+    # Conduct principle components analysis and project onto principle components
+    pca = PCA(n_components=len(returns_df.columns))
+    print(scaled.shape)
+    transformed = pca.fit_transform(scaled)
+
+    eigenvalues = pca.explained_variance_
+    pc_df = pd.DataFrame(pca.components_, columns = ['PC{}'.format(i) for i in range(1,len(returns_df.columns)+1)], index = returns_df.columns)
+    # Divide rows by STDEV of each coin return to get eigen portfolio weights
+    eigenportfolios = pc_df.div(stdev_returns, axis=0)
+    # The columns of 'eigenportfolios' are the relevant eigenportfolios. 
+    # Lets add the returns of the first n eigenportfolios to the returns dataframe.
+    for i in range(1, n+1):
+        pc = 'PC{}'.format(i)
+        returns_df[pc] = np.sum(returns_df[stocks].multiply(eigenportfolios[pc].to_list()),axis=1)/np.sqrt(eigenvalues[i-1])
+    return returns_df
 
 
 # valid = returns.columns
@@ -51,7 +78,7 @@ def build_returns_dataframe(stock_prices: list) -> pd.DataFrame:
 # eigenvalues = pca.explained_variance_
 # pcdf = pd.DataFrame(pca.components_, columns=pcs, index=returns.columns)
 # # Divide rows by STDEV of each coin return to get eigen portfolio weights
-# eig_portfolios = pcdf.div(s,axis=0)
+# eig_portfolios = pcdf.div(s, axis=0)
 # # Eigen portfolio's are the columns. Add returns for each eigen portfolio of interest to pca_data
 # for i in range (1,15+1):
 #     pc = 'PC{}'.format(i)
@@ -72,4 +99,5 @@ def build_returns_dataframe(stock_prices: list) -> pd.DataFrame:
 if __name__ == "__main__":
     stock_data = load_stock_data(PATH_TO_STOCKS_DATA)
     returns_df = build_returns_dataframe(stock_data)
+    returns_df = perform_pca(returns_df, 15)
     print(returns_df.head())
